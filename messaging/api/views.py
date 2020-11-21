f6e04db235bd2cdf132a84f72c1197f2edb2e6c5
Q <q@misell.cymru>
2020-11-21 15:17:32 +0000

    undo

--- messaging/api/views.py
+++ messaging/api/views.py
@@ -57,6 +57,5 @@ class RepresentativeSet(
 
     def perform_create(self, serializer: serializers.RepresentativeSerializer):
         serializer.save(
-            brand_id=self.kwargs['brand_pk'],
-            force_insert=False
+            brand_id=self.kwargs['brand_pk']
         )
